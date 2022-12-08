1412b5dfbff898b6943d790879d2d7d1fd851fec
emman <emmanuelnoronha3@gmail.com>
2022-12-08 09:18:36 -0300

    update - 08/12

--- teste/tasks/urls.py
+++ teste/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('hello/', views.hello),
     path('', views.taskList, name="task-list"),
+    path('task/<int:id>', views.taskView, name="task-view"),
     path('seunome/<str:nome>', views.seuNome, name="SN"),
 ]
--- teste/tasks/views.py
+++ teste/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 # Create your views here.
@@ -13,3 +13,8 @@ def taskList(request):
 
 def seuNome(request, nome):
     return render(request, 'tasks/list.html', {'name':nome})
+
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})

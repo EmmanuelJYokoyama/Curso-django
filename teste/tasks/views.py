db2da7dcdd5a0adeda4b6c5a7b46dcd8a4c5a52c
emman <emmanuelnoronha3@gmail.com>
2022-12-08 08:38:44 -0300

    update 08/12

--- teste/tasks/views.py
+++ teste/tasks/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Task
 # Create your views here.
 def hello(request):
-    return HttpResponse('<Hello world')
+    return HttpResponse('Hello world')
 
 
 def taskList(request):
+    tasks = Task.objects.all()
     return render(request, 'tasks/list.html')
 
 

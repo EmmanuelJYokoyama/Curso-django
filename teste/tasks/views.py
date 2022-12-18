993728e299bf05b46b6f3783c733b4fcf5ddcbd9
emman <emmanuelnoronha3@gmail.com>
2022-12-18 10:46:47 -0300

    update (js added) - 18/12

--- teste/tasks/views.py
+++ teste/tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import Task
 from django.contrib import messages
@@ -10,7 +11,12 @@ def hello(request):
 
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
